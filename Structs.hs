b02106154f6d482f9b0fe6790d2eb14ec30ce632
Alex Burka <aburka@exyntechnologies.com>
2020-04-01 22:38:51 -0400

    data structures

---
+++ Structs.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Structs where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Maybe
+
+data Direction = LEFT | RIGHT | UP | DOWN
+    deriving(Show)
+
+data Register = ACC | Dir Direction
+
+instance Show Register where
+    show ACC = "ACC"
+    show (Dir dir) = show dir
+
+data Operand = Imm Int | Reg Register
+
+instance Show Operand where
+    show (Imm i) = show i
+    show (Reg reg) = show reg
+
+data Instruction =
+      NOP
+    | MOV Operand Operand
+    | SWP
+    | SAV
+    | ADD Operand
+    | SUB Operand
+    | NEG
+    | JMP Label
+    | JEZ Label
+    | JNZ Label
+    | JGZ Label
+    | JLZ Label
+    | JRO Operand
+    deriving(Show)
+
+data Comment = Comment Text
+    deriving(Show)
+
+data Label = Label Text
+
+instance Show Label where
+    show (Label label) = T.unpack label
+
+
+data Line = Line (Maybe Label) (Maybe Instruction) (Maybe Comment)
+    deriving(Show)
+
+data CellProgram = Cell [Line]
+    deriving(Show)
+
+data Program = Program [CellProgram]
+    deriving(Show)
+
+data Cell = Compute | Memory | Damaged
+    deriving(Show)
+
+data Input = Input Text
+    deriving(Show)
+
+data Output = Output Text
+    deriving(Show)
+
+data Machine = Machine [Cell] [(Maybe Input)] [(Maybe Output)]
+    deriving(Show)
+
+printLine (Line label instr comment) =
+    let label_str (Label label) = label <> ":"
+        comment_str (Comment comment) = "# " <> comment
+    in
+        T.unpack $ T.unwords $ catMaybes [label_str <$> label, (T.pack . show) <$> instr, comment_str <$> comment]
+
